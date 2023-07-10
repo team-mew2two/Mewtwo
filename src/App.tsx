cc0fca32cb65a8d852ee55049b90e70d530ec36a
Patrick Vuong <patrickv77@gmail.com>
2023-07-10 17:04:32 -0500

    move techs out of app and into separate folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,12 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+import Techs from './pages/Techs/Techs';
 import './App.css';
 
 function App() {
-  const [technologies, setTechnologies] = useState([
-    'Javascript',
-    'Typescript',
-    'Prometheus',
-    'Grafana',
-    'Hello',
-    'World',
-    'I',
-    'Love',
-    'Shrek',
-    'How',
-    'Many',
-    'More',
-    'Of',
-    'These',
-    'Bro',
-    'What',
-    'In',
-    'Tarnation',
-  ]);
+  
 
   return (
-    <div className="main">
-      <h1>Job Notebook</h1>
-      <div className="pages">
-        <button>tech</button>
-        <button>notes</button>
-      </div>
-      <div className="cols">
-        <div className="techColumn">
-          {technologies.map((el) => (
-            <div className="techs">{el}</div>
-          ))}
-        </div>
-
-        <div className="noteColumn">
-          <div>will uwu pwease wendew me some fwiends?</div>
-        </div>
-      </div>
-    </div>
+    <Techs />
   );
 }
 
