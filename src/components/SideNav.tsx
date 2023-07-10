a8831c4713995d2e2189946670772e7aca581fcd
YeChanGoo <126903033+YeChanGoo@users.noreply.github.com>
2023-07-10 18:09:15 -0400

    feat: notes page with sidenav and topnav

---
+++ src/components/SideNav.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+export function SideNav() {
+  const mockList = ['comp1', 'comp2', 'comp3', 'comp4', 'comp5'];
+  const [count, setCount] = useState(mockList);
+
+  function handleChange(index: number, event: React.ChangeEvent<HTMLInputElement>) {
+    const updatedList = [...count];
+    updatedList[index] = event.target.value;
+    setCount(updatedList);
+  }
+
+  function addComp() {
+    setCount([...count, 'new comp']);
+  }
+
+  return (
+    <>
+      <div className="SideNav">
+        <button onClick={addComp}>Add Comp</button>
+        {count.map((item, index) => (
+          <div key={index}>
+            <input type="text" value={item} onChange={(event) => handleChange(index, event)} className="InputBox" />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
---
+++ src/components/TopNav.tsx
@@ -0,0 +1,14 @@
+export function TopNav() {
+  return (
+    <>
+      <div className="Navigation">
+        <div className="Tech-Nav">
+          <h1>Techs</h1>
+        </div>
+        <div className="Notes-Nav">
+          <h1>Notes</h1>
+        </div>
+      </div>
+    </>
+  )
+}
