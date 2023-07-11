5e77de28ffb74780c5ab2fa1873d6c8c8ff7eee2
davidchuang5 <davidchuang5@gmail.com>
2023-07-11 16:14:46 -0500

    Add test middleware, convert to ESModule syntax

--- src/server/controllers/JobNotebookController.ts
+++ src/server/controllers/JobNotebookController.ts
@@ -1,12 +1,30 @@
-const db = require('../models/JobNotebookModel');
+import db from '../models/JobNotebookModel';
 import { Request, Response, NextFunction } from 'express';
 
 const jobNotebookController = {
   createUser: (req: Request, res: Response, next: NextFunction) => {},
   loginUser: (req: Request, res: Response, next: NextFunction) => {},
   verifyUser: (req: Request, res: Response, next: NextFunction) => {},
+  test: (req: Request, res: Response, next: NextFunction) => {},
 };
 
+jobNotebookController.test = async (req: Request, res: Response, next: NextFunction) => {
+  console.log('----------------------------1313131311313124242423535353464646457575756868686');
+  try {
+    const queryObj = {
+      text: `SELECT * FROM Users`,
+    };
+    const response = await db.query(queryObj.text);
+
+    res.locals.users = response;
+    console.log(response);
+    console.log('----------------------------cdscasdcacacacaca');
+    return next();
+  } catch (error) {
+    console.error(error);
+    return next();
+  }
+};
 jobNotebookController.verifyUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { username } = req.body;
@@ -15,7 +33,7 @@ jobNotebookController.verifyUser = async (req: Request, res: Response, next: Nex
       text: `SELECT EXISTS(SELECT 1 FROM users WHERE username = ${username}) AS user_exists`,
     };
 
-    const userExists = await db.query(queryObj);
+    const userExists = await db.query(queryObj.text);
     userExists
       ? (res.locals.userExists = userExists)
       : (res.locals.userExists = 'user does not exist');
@@ -39,7 +57,7 @@ jobNotebookController.createUser = async (req: Request, res: Response, next: Nex
 
     if (userExists === true) throw new Error(userExists);
 
-    const newUser = await db.query(queryObj);
+    const newUser = await db.query(queryObj.text);
     console.log(newUser);
     res.locals.newUser = newUser;
     return next();
@@ -63,9 +81,9 @@ jobNotebookController.loginUser = async (req: Request, res: Response, next: Next
       WHERE users.id = $1`,
       values: [req.query.id],
     };
-    const allInfo = await db.query(queryObj);
-    const results = await allInfo.json();
-    res.locals.currentUser = results.rows[0];
+    const allInfo = await db.query(queryObj.text);
+    //const results = await allInfo.json();
+    //res.locals.currentUser = results.rows[0];
   } catch (err) {
     console.error(err);
     return next(err);
