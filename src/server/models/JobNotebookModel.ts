8742d49f963559b664052efce58ff0d8a5320087
davidchuang5 <davidchuang5@gmail.com>
2023-07-11 16:15:01 -0500

    Convert to ESModule syntax

--- src/server/models/JobNotebookModel.ts
+++ src/server/models/JobNotebookModel.ts
@@ -1,15 +1,17 @@
-import { Pool, QueryResult } from 'pg';
+import pkg from 'pg';
+import { QueryResult } from 'pg';
+const { Pool } = pkg;
 
 const PG_URI =
-  'postgres://tetntfob:PQo4EHYq5ztPJMNV-DUcAcXaRwV4c2fW@drona.db.elephantsql.com/tetntfob';
+  'postgres://ztzmjwub:yuFlDXd2zGy5fESOHhDH0Ief2PXDqX6J@stampy.db.elephantsql.com/ztzmjwub';
 
 // create a new pool here using the connection string above
 const pool = new Pool({
   connectionString: PG_URI,
 });
 
-module.exports = {
-  query: (text: string, params: any[]): Promise<QueryResult<any>> => {
+export default {
+  query: (text: string, params?: any[]): Promise<QueryResult<any>> => {
     console.log('executed query', text);
     return pool.query(text, params);
   },
